40e99fbf87eb931883ed928186f22b167bd8ce8f
Ailin Nemui <ailin@esf51.localdomain>
2014-08-27 16:39:32 +0200

    Add support for buffer items navigation
    For protocols where one textbuffer is shared by multiple buffers, add
    a mini buffer navigation above the input line.

--- js/glowingbear.js
+++ js/glowingbear.js
@@ -351,6 +351,7 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
     $scope.nicklist = {};
 
     $scope.activeBuffer = models.getActiveBuffer;
+    $scope.bufferItems = models.getActiveBufferItems.bind(models);
 
     $rootScope.connected = false;
     $rootScope.waseverconnected = false;
