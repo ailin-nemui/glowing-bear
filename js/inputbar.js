81c98c78349aef6a8433ca98ef5c55805e3cbfd9
Ailin Nemui <ailin@esf51.localdomain>
2014-09-25 15:02:18 +0200

    simpler approach, no line based scrolling, just use clientHeight

--- js/inputbar.js
+++ js/inputbar.js
@@ -316,24 +316,14 @@ weechat.directive('inputBar', function() {
                         return true;
                     }
                     lines = bufferlines.querySelectorAll("tr");
-                    for (i = lines.length - 1; i >= 0; i--) {
-                        if ((lines[i].offsetTop-bufferlines.scrollTop)<bufferlines.clientHeight/2 && lines[i].offsetHeight+lines[i].offsetTop-bufferlines.scrollTop < bufferlines.clientHeight) {
-                            lines[i].scrollIntoView(false);
-                            break;
-                        }
-                    }
+                    bufferlines.scrollTop -= bufferlines.clientHeight/2;
                     return true;
                 }
 
                 // Page down -> scroll down
                 if ($event.type === "keydown" && code === 34 && document.activeElement === inputNode && !$event.ctrlKey && !$event.altKey && !$event.shiftKey) {
                     lines = bufferlines.querySelectorAll("tr");
-                    for (i = 0; i < lines.length; i++) {
-                        if ((lines[i].offsetTop-bufferlines.scrollTop)>bufferlines.clientHeight/2 && lines[i].offsetHeight+lines[i].offsetTop-bufferlines.scrollTop > 0) {
-                            lines[i].scrollIntoView(true);
-                            break;
-                        }
-                    }
+                    bufferlines.scrollTop += bufferlines.clientHeight/2;
                     return true;
                 }
 
