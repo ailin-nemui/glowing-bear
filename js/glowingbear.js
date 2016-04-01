db61696c6aa478b188a373f0228f3617813c41de
Ailin Nemui <ailin@esf51.localdomain>
2016-04-01 16:05:40 +0200

    [teddy] do not use -noswitch

--- js/glowingbear.js
+++ js/glowingbear.js
@@ -563,13 +563,14 @@ weechat.controller('WeechatCtrl', ['$rootScope', '$scope', '$store', '$timeout',
         if (!$scope.setActiveBuffer(fullName, 'fullName')) {
             // WeeChat 0.4.0+ supports /join -noswitch
             // As Glowing Bear requires 0.4.2+, we don't need to check the version
-            var command = 'join -noswitch';
+            var command = models.version[0] >= 4 ? 'join' : 'join -noswitch';
 
             // Check if it's a query and we need to use /query instead
             if (['#', '&', '+', '!'].indexOf(bufferName.charAt(0)) < 0) {  // these are the characters a channel name can start with (RFC 2813-2813)
                 command = 'query';
                 // WeeChat 1.2+ supports /query -noswitch. See also #577 (different context)
                 if ((models.version[0] == 1 && models.version[1] >= 2) || models.version[1] > 1) {
+		  if (models.version[0] < 4)
                     command += " -noswitch";
                 }
             }
